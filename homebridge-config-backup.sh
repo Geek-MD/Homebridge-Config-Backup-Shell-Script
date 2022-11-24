dde83445e735c0e9da702066ac5692c63e8e938f
Edison Montes M <edison.montes@gmail.com>
2022-11-24 14:30:13 -0300

    Update homebridge-config-backup.sh

--- homebridge-config-backup.sh
+++ homebridge-config-backup.sh
@@ -4,6 +4,7 @@ HOMEBRIDGE_BACKUP_DIR='/<abs_path>/docker/homebridge/backup/'
 LOG_FILE="${HOMEBRIDGE_BACKUP_DIR}backup.log"
 DATE=$(date)
 
+# If backup directory does not exist, then create it.
 if test -d "$HOMEBRIDGE_BACKUP_DIR"
   then
     :
@@ -11,6 +12,7 @@ if test -d "$HOMEBRIDGE_BACKUP_DIR"
     mkdir "$HOMEBRIDGE_BACKUP_DIR"
 fi
 
+# If log file dos not exist, then create it.
 if test -f "$LOG_FILE"
   then
     :
@@ -20,6 +22,7 @@ fi
 
 DOCKER_ID=$(docker container ls --all --quiet --filter "name=homebridge")
 
+# If Homebridge docker container is not running, log that info.
 if [ -z "$DOCKER_ID" ]
   then
     LOG_DATA="${DATE} - Homebridge container not runing."
@@ -27,6 +30,7 @@ if [ -z "$DOCKER_ID" ]
     exit 1
 fi
 
+# Copy config.json from docker container to local.
 DOCKERFILE="${DOCKER_ID}:/homebridge/config.json"
 
 FILE1="${HOMEBRIDGE_BACKUP_DIR}config.json"
@@ -34,8 +38,10 @@ FILE2="${HOMEBRIDGE_BACKUP_DIR}config.bak"
 
 docker cp "$DOCKERFILE" "$FILE1"
 
+# Check md5sum of local config.json
 m1=$(md5sum "$FILE1" | cut -d " " -f1)
 
+# If config.bak does not exist, create it from config.json
 if test -f "$FILE2"
   then
     :
@@ -43,10 +49,12 @@ if test -f "$FILE2"
     cp "$FILE1" "$FILE2"
 fi
 
+# Check md5sum of config.bak
 m2=$(md5sum "$FILE2" | cut -d " " -f1)
 
 BAK="${HOMEBRIDGE_BACKUP_DIR}config-${m2}.bak"
 
+# If config-md5sum.bak does not exist, create it from config.bak
 if test -f "$BAK"
   then
     :
@@ -54,6 +62,7 @@ if test -f "$BAK"
     cp "$FILE2" "$BAK"
 fi
 
+# If log file is empty, add first log data, copy config.json into config.bak, and copy config.bak into config-md5sum.bak
 if [ -s "$LOG_FILE" ]
   then 
     :
@@ -64,6 +73,7 @@ if [ -s "$LOG_FILE" ]
     cp "$FILE2" "$BAK"
     fi
 
+# If md5sum of config.json is different from config.bak, add log data, copy config.json into config.bak, and copy config.bak into config-md5sum.bak
 if [ $m1 == $m2 ]
   then
     :
@@ -74,4 +84,16 @@ if [ $m1 == $m2 ]
     cp "$FILE2" "$BAK"
 fi
 
+# Remove local version of config.json
 rm "$FILE1"
+
+#Check number of backup files
+FILECOUNT=$(ls ${HOMEBRIDGE_BACKUP_DIR}config-*.bak | wc -l)
+
+# If number of backup files is greater than 15, remove backups older than 30 days
+if [ FILECOUNT < 16 ]
+  then
+    :
+  else
+    find ${HOMEBRIDGE_BACKUP_DIR}config-*.bak -mtime +30 -exec rm {} \;
+fi
